5f6fc3aeb5595e7868e96579b78ffdb6894176f3
cuscedu <cuscedu@mac-gv.local>
2017-05-19 14:29:48 +0700

    commit 2

---
+++ MonoAppDemo/myfile1.swift
@@ -0,0 +1,13 @@
+//
+//  myfile1.swift
+//  MonoAppDemo
+//
+//  Created by cuscedu on 5/19/17.
+//  Copyright © 2017 cuscedu. All rights reserved.
+//
+
+import UIKit
+
+class myfile1: NSObject {
+
+}
